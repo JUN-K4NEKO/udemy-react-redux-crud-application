8f0e79568cb2fbf5b7c41d8e0154b0680c9ff400
Jun V&V <jun@voiceandvibes.com>
2020-01-17 13:14:05 +0100

    learn jsx syntax Please enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'; // must import React to use JSX 
+
+// class App extends Component {
+// 	render() {
+// 		return <div>Hello World</div>;
+// 	}
+// }
+
+// without JSX
+// class App extends Component {
+// 	render() {
+// 		return React.createElement(
+// 			"div", 
+// 			null,
+// 			"Hello World!"
+// 		);
+// 	}
+// }
 
 class App extends Component {
-  render() {
-    return <div><h1 >Hello World</h1></div>;
-  }
+	render() {
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input id="bar" type="text" onClick={() => { console.log("I am clicked.") }} />
+      </React.Fragment>
+    )
+	}
 }
 
 export default App;
