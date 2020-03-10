425f016ffe860dbe0eb1504dd47f1aa2b050196e
Jun V&V <jun@voiceandvibes.com>
2020-03-10 17:05:43 +0100

    prop-type

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'; // must import React to use JSX 
+import PropTypes from 'prop-types';
 
 // class App extends Component {
 // 	render() {
@@ -32,7 +33,7 @@ const App = () => {
 	const profiles = [
 		{ name: "Taro", age: 10 },
 		{ name: "Hanako", age: 12 },
-		{ name: "NoName" }
+		{ name: "Noname" }
 	]
 	return <div>
 		{
@@ -46,8 +47,9 @@ const User = (props) => {
 	return <div>Hi, I am {props.name}, and I'm {props.age} years old!</div>
 }
 
-User.defaultProps = {
-	age: 1
+User.propTypes = {
+	name: PropTypes.string,
+	age: PropTypes.number.isRequired
 }
 
 export default App;
