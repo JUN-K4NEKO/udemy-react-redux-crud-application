e2d12cd7e6482becd5fdbb3d51f02746ef7f1c65
Jun V&V <jun@voiceandvibes.com>
2020-03-10 16:25:40 +0100

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'; // must import React to use JSX 
+import React from 'react'; // must import React to use JSX 
 
 // class App extends Component {
 // 	render() {
@@ -17,15 +17,27 @@ import React, { Component } from 'react'; // must import React to use JSX
 // 	}
 // }
 
-class App extends Component {
-	render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input id="bar" type="text" onClick={() => { console.log("I am clicked.") }} />
-      </React.Fragment>
-    )
-	}
+// class App extends Component {
+// 	render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input id="bar" type="text" onClick={() => { console.log("I am clicked.") }} />
+//       </React.Fragment>
+//     )
+// 	}
+// }
+
+const App = () => {
+	return <div>
+		<Cat />
+		<Cat />
+		<Cat />
+		<Cat />
+	</div>
+}
+const Cat = () => {
+	return <div>Meow!</div>
 }
 
 export default App;
