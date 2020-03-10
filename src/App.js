3418f8d834bbc81ed09f9ea3b2f317a44d10f2c6
Jun V&V <jun@voiceandvibes.com>
2020-03-10 16:42:36 +0100

    learn props

--- src/App.js
+++ src/App.js
@@ -29,15 +29,25 @@ import React from 'react'; // must import React to use JSX
 // }
 
 const App = () => {
+	const profiles = [
+		{ name: "Taro", age: 10 },
+		{ name: "Hanako", age: 12 },
+		{ name: "NoName" }
+	]
 	return <div>
-		<Cat />
-		<Cat />
-		<Cat />
-		<Cat />
+		{
+			profiles.map((profile, index) => {
+				return <User name={profile.name} age={profile.age} key={index}/>
+			})
+		}
 	</div>
 }
-const Cat = () => {
-	return <div>Meow!</div>
+const User = (props) => {
+	return <div>Hi, I am {props.name}, and I'm {props.age} years old!</div>
+}
+
+User.defaultProps = {
+	age: 1
 }
 
 export default App;
